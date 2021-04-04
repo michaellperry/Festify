f13e1b210470ce0d1182819c811520cbad14faeb
Michael L Perry <michael@qedcode.com>
2021-04-04 13:38:46 -0500

    Consolidate test setup and cleanup

--- Festify.Sales.UnitTest/SalesStateMachineTests.cs
+++ Festify.Sales.UnitTest/SalesStateMachineTests.cs
@@ -1,40 +1,53 @@
 using System;
 using System.Threading.Tasks;
+
 using Festify.Sales.Messages;
 using Festify.Sales.States;
+
 using FluentAssertions;
+
 using MassTransit.Testing;
+
 using Xunit;
 
 namespace Festify.Sales.UnitTest
 {
-    public class SalesStateMachineTests
+    public class SalesStateMachineTests : IAsyncLifetime
     {
+        private readonly SalesStateMachine machine;
+        private readonly InMemoryTestHarness harness;
+        private readonly StateMachineSagaTestHarness<SalesState, SalesStateMachine> sagaHarness;
+
+        public SalesStateMachineTests()
+        {
+            machine = new SalesStateMachine();
+            harness = new InMemoryTestHarness();
+            sagaHarness = harness.StateMachineSaga<SalesState, SalesStateMachine>(machine);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await harness.Start();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await harness.Stop();
+        }
+
         [Fact]
         public async Task WhenPurchaseSubmitted_SaleIsStarted()
         {
-            var machine = new SalesStateMachine();
-            var harness = new InMemoryTestHarness();
-            var sagaHarness = harness.StateMachineSaga<SalesState, SalesStateMachine>(machine);
-
-            await harness.Start();
-            try
+            var purchaseGuid = Guid.NewGuid();
+            await harness.Bus.Publish(new PurchaseSubmitted
             {
-                var purchaseGuid = Guid.NewGuid();
-                await harness.Bus.Publish(new PurchaseSubmitted
-                {
-                    PurchaseGuid = purchaseGuid
-                });
+                PurchaseGuid = purchaseGuid
+            });
 
-                harness.Consumed.Select<PurchaseSubmitted>().Should().HaveCount(1);
-                sagaHarness.Consumed.Select<PurchaseSubmitted>().Should().HaveCount(1);
-                sagaHarness.Created.ContainsInState(purchaseGuid, machine, machine.Started)
-                    .Should().NotBeNull();
-            }
-            finally
-            {
-                await harness.Stop();
-            }
+            harness.Consumed.Select<PurchaseSubmitted>().Should().HaveCount(1);
+            sagaHarness.Consumed.Select<PurchaseSubmitted>().Should().HaveCount(1);
+            sagaHarness.Created.ContainsInState(purchaseGuid, machine, machine.Started)
+                .Should().NotBeNull();
         }
     }
 }
