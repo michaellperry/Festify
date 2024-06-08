249d402343743b8d09f0e6d9d645b0a8c64429b3
Michael L Perry <michael@qedcode.com>
2024-06-08 15:17:14 -0500

    Format Act queries

--- Festify.Promotion/Acts/ActQueries.cs
+++ Festify.Promotion/Acts/ActQueries.cs
@@ -10,51 +10,51 @@ public class ActQueries
 
     public ActQueries(PromotionContext repository)
     {
-            this.repository = repository;
-        }
+        this.repository = repository;
+    }
 
     public async Task<List<ActInfo>> ListActs()
     {
-            var result = await repository.Set<Act>()
-                .Where(act => !act.Removed.Any())
-                .Select(act => new
-                {
-                    act.ActGuid,
-                    Description = act.Descriptions
-                        .OrderByDescending(d => d.ModifiedDate)
-                        .FirstOrDefault()
-                })
-                .ToListAsync();
+        var result = await repository.Set<Act>()
+            .Where(act => !act.Removed.Any())
+            .Select(act => new
+            {
+                act.ActGuid,
+                Description = act.Descriptions
+                    .OrderByDescending(d => d.ModifiedDate)
+                    .FirstOrDefault()
+            })
+            .ToListAsync();
 
-            return result
-                .Select(row => MapActModel(row.ActGuid, row.Description))
-                .ToList();
-        }
+        return result
+            .Select(row => MapActModel(row.ActGuid, row.Description))
+            .ToList();
+    }
 
     public async Task<ActInfo> GetAct(Guid actGuid)
     {
-            var result = await repository.Set<Act>()
-                .Where(act => act.ActGuid == actGuid)
-                .Select(act => new
-                {
-                    act.ActGuid,
-                    Description = act.Descriptions
-                        .OrderByDescending(d => d.ModifiedDate)
-                        .FirstOrDefault()
-                })
-                .SingleOrDefaultAsync();
+        var result = await repository.Set<Act>()
+            .Where(act => act.ActGuid == actGuid)
+            .Select(act => new
+            {
+                act.ActGuid,
+                Description = act.Descriptions
+                    .OrderByDescending(d => d.ModifiedDate)
+                    .FirstOrDefault()
+            })
+            .SingleOrDefaultAsync();
 
-            return result == null ? null : MapActModel(result.ActGuid, result.Description);
-        }
+        return result == null ? null : MapActModel(result.ActGuid, result.Description);
+    }
 
     private static ActInfo MapActModel(Guid actGuid, ActDescription actDescription)
     {
-            return new ActInfo
-            {
-                ActGuid = actGuid,
-                Title = actDescription?.Title,
-                ImageHash = actDescription?.ImageHash,
-                LastModifiedTicks = actDescription?.ModifiedDate.Ticks ?? 0
-            };
-        }
+        return new ActInfo
+        {
+            ActGuid = actGuid,
+            Title = actDescription?.Title,
+            ImageHash = actDescription?.ImageHash,
+            LastModifiedTicks = actDescription?.ModifiedDate.Ticks ?? 0
+        };
+    }
 }
